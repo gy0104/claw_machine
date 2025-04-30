28dd18d4e802d3f7aac1a6815516c8b76a973a34
gayoungpark <gayoung2014@gmail.com>
2025-04-30 16:33:32 +0900

    index.js 제목 스타일 수정 및 이모지 추가

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main style={{ padding: '20px', textAlign: 'center', fontFamily: 'Apple SD Gothic Neo, sans-serif' }}>
       <h1 style={{ fontSize: '22px', fontWeight: 600 }}>
-        ♡ 주석이를 위한 Claw Machine 위치 ♡
+        주석이를 위한 Claw Machine 위치 ♡
       </h1>
       <KakaoMap />
     </main>
