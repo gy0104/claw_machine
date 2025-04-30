7228c217929299bdb4fe7004e27d73dd9a8f8b4e
gayoungpark <gayoung2014@gmail.com>
2025-04-30 16:28:09 +0900

    동기 script 제거 - _document.js 수정

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,10 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head>
-        <script
-          src="https://apis.openapi.sk.com/tmap/jsv2?version=1&appKey=4d8MGx5fMP8A2VMYKllG17EvPk1rtxN8VHksSkH6"
-          type="text/javascript"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
