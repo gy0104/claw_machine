4ae97ef01bb123ec834e3890101200da36069eda
gayoungpark <gayoung2014@gmail.com>
2025-05-02 15:41:36 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,15 @@
+// next.config.js
 import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 다른 설정 필요시 여기에 추가
+  // 필요한 경우 다른 설정 추가
 };
 
 export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development', // 개발 중엔 비활성화
 })(nextConfig);
